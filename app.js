23eb4bad44af8c47074feb47d72f78b48079ade5
Chris Patrick <atomize101@gmail.com>
2020-11-13 14:03:42 -0500

    Adding listeners for the boxes and functions to console log when being dragged

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ document.addEventListener('DOMContentLoaded', () => {
 	function createBoard() {
 		for (let i = 0; i < width * width; i++) {
 			const square = document.createElement('div');
+			square.setAttribute('draggable', true);
+			square.setAttribute('id', i);
 			let randomColor = Math.floor(Math.random() * candyColors.length);
 			square.style.backgroundColor = candyColors[randomColor];
 			grid.appendChild(square);
@@ -16,4 +18,31 @@ document.addEventListener('DOMContentLoaded', () => {
 		}
 	}
 	createBoard();
+
+	//Drag the candies
+	squares.forEach((square) => square.addEventListener('dragstart', dragStart));
+	squares.forEach((square) => square.addEventListener('dragend', dragEnd));
+	squares.forEach((square) => square.addEventListener('dragover', dragOver));
+	squares.forEach((square) => square.addEventListener('dragenter', dragEnter));
+	squares.forEach((square) => square.addEventListener('dragleave', dragLeave));
+	squares.forEach((square) => square.addEventListener('drop', dragDrop));
+
+	function dragStart() {
+		console.log(this.id, 'dragstart');
+	}
+	function dragOver() {
+		console.log(this.id, 'dragover');
+	}
+	function dragEnter() {
+		console.log(this.id, 'dragenter');
+	}
+	function dragLeave() {
+		console.log(this.id, 'dragleave');
+	}
+	function dragEnd() {
+		console.log(this.id, 'dragend');
+	}
+	function dragDrop() {
+		console.log(this.id, 'dragdrop');
+	}
 });

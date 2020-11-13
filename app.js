3bdbd16a75563e71269a640d02e8bc89463edc0e
Chris Patrick <atomize101@gmail.com>
2020-11-13 12:42:25 -0500

    Boxes now random colors when board is created

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ document.addEventListener('DOMContentLoaded', () => {
 	const width = 8;
 	const squares = [];
 
+	const candyColors = ['red', 'yellow', 'orange', 'purple', 'green', 'blue'];
+
 	//Create Board
 	function createBoard() {
 		for (let i = 0; i < width * width; i++) {
 			const square = document.createElement('div');
+			let randomColor = Math.floor(Math.random() * candyColors.length);
+			square.style.backgroundColor = candyColors[randomColor];
 			grid.appendChild(square);
 			squares.push(square);
 		}
